711b2b0554f1a7776d69dbeeaa50ed93c89b8c35
scaricopax8 <50639783+scaricopax8@users.noreply.github.com>
2022-05-09 10:21:42 -0600

    initial commit

---
+++ instrumented/main.js
@@ -0,0 +1,51 @@
+function cov_prxc401pg() {
+  var path = "/Users/pax8-scarico-02/Development/nyc-vue-3/src/main.js";
+  var hash = "2c7c0d5aff2d726813c375d74a54afd4ce590719";
+  var global = new Function("return this")();
+  var gcv = "__coverage__";
+  var coverageData = {
+    path: "/Users/pax8-scarico-02/Development/nyc-vue-3/src/main.js",
+    statementMap: {
+      "0": {
+        start: {
+          line: 4,
+          column: 0
+        },
+        end: {
+          line: 4,
+          column: 28
+        }
+      }
+    },
+    fnMap: {},
+    branchMap: {},
+    s: {
+      "0": 0
+    },
+    f: {},
+    b: {},
+    _coverageSchema: "1a1c01bbd47fc00a2c39e90264f33305004495a9",
+    hash: "2c7c0d5aff2d726813c375d74a54afd4ce590719"
+  };
+  var coverage = global[gcv] || (global[gcv] = {});
+
+  if (!coverage[path] || coverage[path].hash !== hash) {
+    coverage[path] = coverageData;
+  }
+
+  var actualCoverage = coverage[path];
+  {
+    // @ts-ignore
+    cov_prxc401pg = function () {
+      return actualCoverage;
+    };
+  }
+  return actualCoverage;
+}
+
+cov_prxc401pg();
+import { createApp } from 'vue';
+import App from './App.vue';
+cov_prxc401pg().s[0]++;
+createApp(App).mount('#app');
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6WyJjcmVhdGVBcHAiLCJBcHAiLCJtb3VudCJdLCJzb3VyY2VzIjpbIm1haW4uanMiXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHsgY3JlYXRlQXBwIH0gZnJvbSAndnVlJ1xuaW1wb3J0IEFwcCBmcm9tICcuL0FwcC52dWUnXG5cbmNyZWF0ZUFwcChBcHApLm1vdW50KCcjYXBwJylcbiJdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7OztJQWVZOzs7Ozs7Ozs7QUFmWixTQUFTQSxTQUFULFFBQTBCLEtBQTFCO0FBQ0EsT0FBT0MsR0FBUCxNQUFnQixXQUFoQjs7QUFFQUQsU0FBUyxDQUFDQyxHQUFELENBQVQsQ0FBZUMsS0FBZixDQUFxQixNQUFyQiJ9
